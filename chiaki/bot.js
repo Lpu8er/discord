8a07689999b4bbd9f1b2e0bbcaaa26261f8998e1
Lpu8er <lpu8er@gmail.com>
2018-10-03 19:56:59 +0200

    Functional starting code, youhou

--- chiaki/bot.js
+++ chiaki/bot.js
@@ -5,6 +5,9 @@ class Bot {
         var Client = require('node-rest-client').Client;
         this.restClient = new Client;
         this.W3CWebSocket = require('websocket').w3cwebsocket;
+        this.connectionStatus = 0;
+        this.heartbeatHandler = null;
+        this.lastSequenceNumber = null;
     }
     
     start() {
@@ -40,14 +43,60 @@ class Bot {
     
     triggerHappy() {
         if(null !== this.gatewayUri && 0 < this.guilds.length) {
-            console.log(this.gatewayUri);
-            this.wsc = new this.W3CWebSocket(this.gatewayUri);
-            this.wsc.onopen = this.doStuff;
+            this.openConnection();
         }
     }
     
-    doStuff(cx) {
+    openConnection() {
+        this.connectionStatus = 1; // connecting
+        this.wsc = new this.W3CWebSocket(this.gatewayUri);
+        this.wsc.onopen = (e) => { this.connected(e); };
+        this.wsc.onmessage = (e) => { this.read(e); };
+        this.wsc.onerror = function(e){console.log('ERROR'); console.log(e);};
+    }
+    
+    send(message, op, e, s) {
+        this.wsc.send(JSON.stringify({
+            'op': op,
+            'd': message,
+            's': s || 0,
+            't': e
+        }));
+    }
+    
+    heartbeat(interval) {
+        this.send(this.lastSequenceNumber, 1, 'HEARTBEAT');
+        this.heartbeatHandler = setTimeout(() => { this.heartbeat(interval); }, interval);
+    }
+    
+    stopHeartbeat() {
+        clearTimeout(this.heartbeatHandler);
+    }
+    
+    connected(e) {
+        this.connectionStatus = 2; // connected, not identified
         console.log('Connected to gateway.');
+    }
+    
+    read(e) {
+        if('message' === e.type) { // readable
+            let dd = JSON.parse(e.data);
+            if(10 === dd.op) {
+                if(dd.d.hasOwnProperty('heartbeat_interval')) {
+                    if(dd.s) {
+                        this.lastSequenceNumber = dd.s;
+                    }
+                    // start heartbeating
+                    this.heartbeat(parseInt(dd.d.heartbeat_interval));
+                    // let's identify
+                    this.connectionStatus = 3; // identifying
+                    this.send({
+                        'token': this.config.token,
+                        'properties': {'$os': 'linux', '$browser': 'cli', '$device': 'node'}
+                    }, 2, 'IDENTIFY');
+                } // else wtf ?
+            }
+        }
     }
 };
 
